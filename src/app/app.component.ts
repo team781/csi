65c1c9db53923d9f2dbfff30eceef780665e3b4c
team781 <team781123@gmail.com>
2019-12-29 22:41:21 +0100

    changement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-csi';
+  scrolltop : boolean = false
+
+  @HostListener("window: scroll",["$event"]) show(){
+    console.log(window)
+    if(window.scrollY > 150) this.scrolltop = true
+    else this.scrolltop = false
+  }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { $ } from 'protractor';
 
 @Component({
@@ -7,12 +7,19 @@ import { $ } from 'protractor';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
   show: boolean = false
 
+  scrolled : boolean = false
+  @HostListener("window:scroll") scroll(){
+    if(window.scrollY > 80) this.scrolled = true
+    else this.scrolled = false
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+export class NgbdCarouselBasic {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
