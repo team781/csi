2c9257d911e980e0cf216fed7fc60ac95a6b315e
team781 <team781@gmail.com>
2020-01-08 23:34:28 +0100

    firas

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import jump from "jump.js"
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  goFormation(){
+    jump("#formation")
+  }
+  scrollTop(){
+    jump(- window.scrollY)
+  }
   constructor() { }
 
   ngOnInit() {
